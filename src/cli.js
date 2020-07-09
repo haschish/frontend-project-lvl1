9189e9f3b726a03a41bdbfaa30ecbcb812aa82c0
haschish <haschish@gmail.com>
2020-07-09 20:02:10 +0500

    step 4.2

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,3 @@
 import readLineSync from 'readline-sync';
 
-export const askName = () => {
-	return readLineSync.question('May I have your name? ');
-}
+export const askName = () => readLineSync.question('May I have your name? ');
